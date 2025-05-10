f10772fe62860f586d86f38f80ad0c501fc0058c
MandredK <mandredking@gmail.com>
2025-05-11 01:04:05 +0530

    A2C method done :>>

--- a2c_pgm.py
+++ a2c_pgm.py
@@ -1,10 +1,11 @@
-# REINFORCE method for Cartpole. Converged.
+# A2C method for Cartpole. Converged under 30 min on gtx 1650.
 
 import numpy as np
 import gymnasium as gym
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 from torch.utils.tensorboard.writer import SummaryWriter
 import collections
 import time
@@ -13,18 +14,24 @@ import time
 class NN(nn.Module):
     def __init__(self, input_size, n_actions):
         super().__init__()
-        self.net = nn.Sequential(
+        self.shared = nn.Sequential( # Common Body
             nn.Linear(input_size, 128),
-            nn.ReLU(),
+            nn.ReLU()
+        )
+        self.policy_head = nn.Sequential( # Actor Head
             nn.Linear(128, n_actions),
             nn.Softmax(dim=1)
         )
+        self.value_head = nn.Linear(128, 1) # Critic Head
 
     def forward(self, x):
-        return self.net(x)
+        base = self.shared(x)
+        policy = self.policy_head(base)
+        value = self.value_head(base)
+        return policy, value
 
 
-class REINFORCE:
+class A2C:
     def __init__(self, env: gym.Env, net: NN):
         self.state = []
         self.action = []
@@ -52,7 +59,8 @@ class REINFORCE:
         state, _ = self.env.reset()
         while True:
             s_tensor = torch.as_tensor(state, dtype=torch.float32).unsqueeze(0).to(DEVICE)
-            probs = self.net(s_tensor).squeeze(0).cpu().detach().numpy()
+            policy,_=self.net(s_tensor)
+            probs = policy.squeeze(0).cpu().detach().numpy()
             action = np.random.choice(N_ACTIONS, p=probs)
 
             self.state.append(state)
@@ -76,6 +84,7 @@ if __name__ == "__main__":
     GAMMA = 0.99
     LEARNING_RATE = 0.01
     EPISODES_BEFORE_TRAINING = 4
+    ENTROPY_BETA=0.01
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Training on {DEVICE}")
 
@@ -87,7 +96,7 @@ if __name__ == "__main__":
 
     optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE)
 
-    reinforce = REINFORCE(env, net)
+    a2c_agent = A2C(env, net)
 
     batch_states, batch_qvals, batch_actions = [], [], []
 
@@ -100,21 +109,21 @@ if __name__ == "__main__":
     start_time = time.time()
 
     while True:
-        q_v, s, a, t_r = reinforce.play_episode()
+        q_v, s, a, t_r = a2c_agent.play_episode()
 
         reward_deque.append(t_r)
         mean_reward=sum(reward_deque)/len(reward_deque)
         print(f"Mean Reward: {mean_reward}")
 
         if mean_reward > 490:
-            print(f"Solved in {reinforce.episode_no} Episodes")
+            print(f"Solved in {a2c_agent.episode_no} Episodes")
             break
 
         batch_states.append(s)
         batch_qvals.append(q_v)
         batch_actions.append(a)
 
-        if reinforce.episode_no % EPISODES_BEFORE_TRAINING == 0:
+        if a2c_agent.episode_no % EPISODES_BEFORE_TRAINING == 0:
             optimizer.zero_grad()
 
             # Flatten batches
@@ -125,18 +134,31 @@ if __name__ == "__main__":
             states_v = torch.FloatTensor(flat_states).to(DEVICE)
             actions_v = torch.LongTensor(flat_actions).to(DEVICE)
             qvals_v = torch.FloatTensor(flat_qvals).to(DEVICE)
+            
             qvals_v = (qvals_v - qvals_v.mean()) / (qvals_v.std() + 1e-8) # Normalization to reduce variance
 
-            logits_v = net(states_v)
-            log_probs = torch.log(logits_v + 1e-8) # Small value to avoid log(0)
-            selected_log_probs = log_probs[range(len(actions_v)), actions_v]
-            loss = -(selected_log_probs * qvals_v).mean()
+            logits_v, value_v = net(states_v)
+            
+            loss_value_v=F.mse_loss(value_v.squeeze(-1),qvals_v) # Value or Critic loss
 
-            loss.backward()
+            log_probs = torch.log(logits_v + 1e-8) # Small value to avoid log(0)
+            adv_v=qvals_v-value_v.squeeze(-1).detach() # Squeeze to remove last dim if 1 ## Advantage value
+            log_prob_actions_v = adv_v * log_probs[range(len(actions_v)), actions_v]
+            loss_policy_v = -log_prob_actions_v.mean() # Policy or Actor loss
+
+            entropy_loss_v = -ENTROPY_BETA * (logits_v * log_probs).sum(dim=1).mean() # Entropy loss
+
+            loss_total=loss_value_v+loss_policy_v+entropy_loss_v # Total loss
+
+            loss_total.backward()
             optimizer.step()
+
             writer.add_scalar("reward_mean", mean_reward, steps)
-            writer.add_scalar("episode_no", reinforce.episode_no, steps)
-            writer.add_scalar("loss", loss, steps)
+            writer.add_scalar("episode_no", a2c_agent.episode_no, steps)
+            writer.add_scalar("loss_value(critic)", loss_value_v, steps)
+            writer.add_scalar("loss_policy(actor)", loss_policy_v, steps)
+            writer.add_scalar("loss_entropy", entropy_loss_v, steps)
+            writer.add_scalar("loss", loss_total, steps)
 
             batch_states, batch_qvals, batch_actions = [], [], []
         
